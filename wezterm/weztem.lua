5d0152609a749a380b13d1a09ef178b8ecb0874b
Isaiah Harville <harvillerisaiah@gmail.com>
2024-10-08 14:22:50 -0500

    add wezterm  conf

---
+++ wezterm/weztem.lua
@@ -0,0 +1,10 @@
+local wezterm = require 'wezterm'
+
+return {
+  font = wezterm.font('JetBrains Mono'),
+  font_size = 12.0, 
+  color_scheme = 'Builtin Solarized Dark', 
+  window_background_opacity = 0.97, 
+  hide_tab_bar_if_only_one_tab = true,
+}
+

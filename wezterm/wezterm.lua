ebd85fea08ab301b56ad4c50ad10ac1689135c42
Isaiah Harville <harvillerisaiah@gmail.com>
2024-11-20 09:07:27 -0600

    disable wezterm tmux bindings

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,14 +1,13 @@
 local wezterm = require 'wezterm'
 
-
 local function in_tmux()
   -- return os.getenv("TMUX") ~= nil
-  return true; -- the env var doesnt work idky 
+  return 1
 end
 
-return {
+local config = {
   font = wezterm.font('JetBrains Mono'),
-  font_size = 13.0, 
+  font_size = 13.0,
   color_scheme = 'Catppuccin Macchiato', -- mocha, frappe, latte
   window_background_opacity = 0.93,
   window_background_gradient = {
@@ -17,9 +16,11 @@ return {
   },
   hide_tab_bar_if_only_one_tab = true,
   enable_scroll_bar = true,
+}
 
-  leader = { key = "b", mods = "CTRL" },
-  keys = not in_tmux() and {
+if not in_tmux() then
+  config.leader = { key = "b", mods = "CTRL" }
+  config.keys = {
     -- Splits
     { key = "%", mods = "LEADER", action = wezterm.action.SplitVertical { domain = "CurrentPaneDomain" } },
     { key = '"', mods = "LEADER", action = wezterm.action.SplitHorizontal { domain = "CurrentPaneDomain" } },
@@ -39,6 +40,8 @@ return {
     { key = "j", mods = "LEADER", action = wezterm.action.AdjustPaneSize { "Down", 5 } },
     { key = "h", mods = "LEADER", action = wezterm.action.AdjustPaneSize { "Left", 5 } },
     { key = "k", mods = "LEADER", action = wezterm.action.AdjustPaneSize { "Up", 5 } },
-  } or {}, 
-}
+  }
+end
+
+return config
 
